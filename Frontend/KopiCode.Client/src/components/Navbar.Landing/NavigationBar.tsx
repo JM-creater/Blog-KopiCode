7adc416b1b75b4808d246d153b3f2d5d640420a4
Racel <racelanne19@gmail.com>
2024-06-03 13:40:41 +0800

    Frontend New Update
    Added the about us page, made the previous web pages and currently added web page responsive.
    Updated the navigation bar.

--- Frontend/KopiCode.Client/src/components/Navbar.Landing/NavigationBar.tsx
+++ Frontend/KopiCode.Client/src/components/Navbar.Landing/NavigationBar.tsx
@@ -4,14 +4,36 @@ import WebLogo from '../logo-image/dark-logo.png';
 import OutlinedButton from '../Buttons/OutlinedButton';
 import TextButton from '../Buttons/TextButton';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 // import { Link, Outlet } from 'react-router-dom';
 
+//to make the navbar transparent when scrolling down
 export default function NavigationBar (){
-    return <nav>
+    useEffect(() => {
+        const handleScroll = () => {
+            const nav = document.getElementById('navbar');
+            // console.log('scroll event fired, window.scrollY:', window.scrollY); //debugging log
+            if (window.scrollY > 0) {
+                //console.log('Adding transparent class'); // Debugging log
+                nav?.classList.add('transparent');
+            } else {
+                //console.log('Removing transparent class'); // Debugging log
+                nav?.classList.remove('transparent');
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+    };
+},[]);
+
+    return (
+    <nav id='navbar'>
         <Link to='/'><img src={WebLogo} alt="web logo dark color" id='webLogo' /></Link>
         <div className="nav__buttonGroup">
             <Link to='/aboutUs'><TextButton label={'About Us'}/></Link>
             <Link to='/login'><OutlinedButton label={'Sign In'}/></Link>
         </div>
     </nav>
+    );
 }
